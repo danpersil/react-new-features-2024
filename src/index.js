7e391ce256b71abbcefc73c98325b91ddefad86d
Daniel Pereira <danielpersilva@gmail.com>
2024-01-26 09:43:01 +0000

    A boiler place with new features
    An easy way to start using react

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NoteApp from './components/NoteApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<NoteApp />);
